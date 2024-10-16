0474c05fe8cb0ab18c6f47a735911ddbc36b3e18
Sumit Rana <ranasumit778@gmail.com>
2024-10-16 22:55:45 +0530

    react-app-url-update

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 //const BASE_URL = "https://studynotion-pv3mcb6zj-yash-chauhans-projects-1816ec77.vercel.app/api/v1"
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = process.env.REACT_APP_BASE_URL ;
 
 // AUTH ENDPOINTS
 export const endpoints = {
